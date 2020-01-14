5bfb64cfb59edfabeff73fc10fa12a44060d00f6
singh <nikita.e.singh@capgemini.com>
2020-01-14 17:12:28 +0530

    Updates

---
+++ precache-manifest.abd578397a57dd33ea0e960dc64e7fb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b729fc0576e4f3d0b121ee0b05e4e64",
+    "url": "/Nikita-Singh123/Cart-Application.git/index.html"
+  },
+  {
+    "revision": "b173db4de29dd79fc64e",
+    "url": "/Nikita-Singh123/Cart-Application.git/static/css/2.a3da14a2.chunk.css"
+  },
+  {
+    "revision": "0c988a7ea489371257cb",
+    "url": "/Nikita-Singh123/Cart-Application.git/static/css/main.cb0c28ab.chunk.css"
+  },
+  {
+    "revision": "b173db4de29dd79fc64e",
+    "url": "/Nikita-Singh123/Cart-Application.git/static/js/2.55385050.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/Nikita-Singh123/Cart-Application.git/static/js/2.55385050.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0c988a7ea489371257cb",
+    "url": "/Nikita-Singh123/Cart-Application.git/static/js/main.10f2759b.chunk.js"
+  },
+  {
+    "revision": "fd6c2fd7207bfa433d0d",
+    "url": "/Nikita-Singh123/Cart-Application.git/static/js/runtime-main.b2e953db.js"
+  }
+]);
